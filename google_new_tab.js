a0f71000087c3ce3f902b927146c943615da641d
whuangbh <whuangbh@connect.ust.hk>
2023-08-30 18:51:48 +1000

    ver 1.2

--- google_new_tab.js
+++ google_new_tab.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Google search result open in new tab
-// @version      1.1
+// @version      1.2
 // @description  As the name suggests, open the google search result in a new tab
 // @author       Hw
 // @match        https://www.google.com/search?*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @namespace    http://tampermonkey.net/
 // ==/UserScript==
 
 (function () {
@@ -14,21 +15,19 @@
 
     // big anchor tag: div.yuRUbf>div>a
     // youtube link: a.X5OiLe
-    // sm anchor tag: a.fl,
+    // sm anchor tag: a.fl, div.HiHjCd>a
 
     const querySelector = [
         "div.yuRUbf>div>a",
         "a.X5OiLe",
         "a.fl",
-        "div.wQiwMc.related-question-pair a",
+        "div.HiHjCd>a",
     ];
-    // const arr = [];
 
     for (const query of querySelector) $(query).attr("target", "_blank");
 
-    // const apply = (arr) => {
-    //     for (const item of arr) $(item).attr("target", "_blank");
-    // };
-
-    // apply(arr);
+    // Expandable button
+    $("div.dnXCYb").click(() => {
+        $("div.yuRUbf>div>a").attr("target", "_blank");
+    });
 })();
