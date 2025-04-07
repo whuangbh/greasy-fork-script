3a659ec5c2843e964fe3b364f7f909bb7910bdc7
Roy Huang <whuangbh@connect.ust.hk>
2025-04-07 16:57:27 +1000

    feat: try to support mobile

--- google_new_tab.js
+++ google_new_tab.js
@@ -1,28 +1,43 @@
 // ==UserScript==
 // @name         Google search result open in new tab
-// @version      1.31
+// @version      1.40
 // @description  As the name suggests, open the google search result in a new tab
 // @author       Hw
 // @match        https://www.google.com/search?*
+// @match        https://www.google.com.*/search?*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @namespace    http://tampermonkey.net/
 // ==/UserScript==
 
 (function () {
-	"use strict";
+    "use strict";
 
-	$("div#rcnt").click((event) => {
-		let target = event.target;
+    document.body.addEventListener(
+        "click",
+        function (event) {
+            const link = event.target.closest("a");
 
-		while (target && target !== document) {
-			if (target.tagName === "A") {
-				event.preventDefault();
-				window.open(target.href, "_blank");
-				break;
-			}
-			target = target.parentElement;
-		}
-	});
+            if (link && link.href) {
+                const isResultLink = link.querySelector("h3");
+                const isInResultsArea =
+                    link.closest("#search") || link.closest("#main");
+
+                if (
+                    isInResultsArea &&
+                    isResultLink &&
+                    link.href.startsWith("http")
+                ) {
+                    if (link.pathname === "/search") {
+                        return;
+                    }
+
+                    event.preventDefault();
+                    event.stopPropagation();
+                    window.open(link.href, "_blank");
+                }
+            }
+        },
+        true
+    );
 })();
